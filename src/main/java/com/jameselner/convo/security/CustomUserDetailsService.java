9d191d811663f850b5b73d245300698279d35d6b
James Elner <elner.k.james@gmail.com>
2025-11-05 16:42:47 +0000

    add security to backend

---
+++ src/main/java/com/jameselner/convo/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.jameselner.convo.security;
+
+import com.jameselner.convo.model.User;
+import com.jameselner.convo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
