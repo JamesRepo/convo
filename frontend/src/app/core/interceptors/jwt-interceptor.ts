dcbcee0d15797f3f2a3d58ba9d5403cfef420f7a
James Elner <elner.k.james@gmail.com>
2025-11-11 21:55:24 +0000

    add auth guard and interceptor

---
+++ frontend/src/app/core/interceptors/jwt-interceptor.ts
@@ -0,0 +1,25 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  // Don't add token to auth endpoints
+  if (req.url.includes('/auth/')) {
+    return next(req);
+  }
+
+  // Add token to other requests
+  if (token && !authService.isTokenExpired()) {
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedRequest);
+  }
+
+  return next(req);
+};
